4bc38f2b6563a6d432aa63fd839988955b02f042
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-29 13:07:51 +0000

    Deploy Digiox/my-contribution to github.com/Digiox/my-contribution.git:gh-pages

---
+++ src/jest.config.js
@@ -0,0 +1,7 @@
+// moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node']
+
+module.exports = {
+  // ...
+  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  // ...
+};
