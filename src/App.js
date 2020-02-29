8a36feea2e12c16635a501f6a4819068af6ac2c2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-29 16:15:40 +0000

    Deploy Digiox/my-contribution to github.com/Digiox/my-contribution.git:gh-pages

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import UnknownPath from './components/Errors/UnknownPath';
+import Main from './components/Main/Main';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+        <Main />
+        </Route>
+        <Route  path="*">
+        <UnknownPath />
+        </Route>
+      </Switch>
+  </Router>
+  );
+}
+
+export default App;
